14475afc72614546ea100c8d1de3713e6d3672ff
Nicholas Smith <nsmith@dalet.com>
2023-03-04 16:14:03 +0000

    Initial version

---
+++ src/test/java/com/example/demo/ExampleControllerTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2020 Dalet - All Rights Reserved
+ *
+ * This file is part of Ooyala Flex.
+ *
+ * Unauthorized copying and/or distribution of this file or any other part of Ooyala Flex, via any medium,
+ * is strictly prohibited.  Proprietary and confidential.
+ */
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = ExampleController.class)
+class ExampleControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Test
+    public void example() throws Exception {
+
+        this.mockMvc
+                .perform(
+                        get("/api/example"))
+                .andExpect(status().isOk());
+    }
+
+}
